f2446112664391a406771c40ac4de662e2eb9262
elrondurpg <elrond.urpg@gmail.com>
2025-06-21 16:00:00 -0400

    updated app.js name and updated start.sh to kill existing process if necessary

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
+pname="node urpg-battle-bot-app.js"
+
+pid=`/bin/ps -fu $USER|grep "$pname" | grep -v "grep" | awk '{print $2}'`
+if [[ $pid -gt 0 ]]; then
+  kill $pid
+  while kill -0 $pid; do
+    sleep 1
+  done
+fi
+
 yum install -y nodejs npm
 cd /home/ec2-user/urpg-battle-bot
 cp /home/ec2-user/.env /home/ec2-user/urpg-battle-bot
