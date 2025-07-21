4d6cd5d21d1e74ee018ef5c4f3d34110e4f30b8c
elrondurpg <elrond.urpg@gmail.com>
2025-07-21 15:50:28 -0400

    updating application deployment scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-pname="node urpg-battle-bot-app.js"
+#pname="node urpg-battle-bot-app.js"
 
-pid=`/bin/ps -fu $USER|grep "$pname" | grep -v "grep" | awk '{print $2}'`
-if [[ $pid -gt 0 ]]; then
-  kill $pid
-  while kill -0 $pid; do
-    sleep 1
-  done
-fi
+#pid=`/bin/ps -fu $USER|grep "$pname" | grep -v "grep" | awk '{print $2}'`
+#if [[ $pid -gt 0 ]]; then
+#  kill $pid
+#  while kill -0 $pid; do
+#    sleep 1
+#  done
+#fi
 
-yum install -y nodejs npm
-cd /etc/battlebot /urpg-battle-bot
-cp /etc/battlebot/.env /etc/battlebot/urpg-battle-bot
-npm install
+#yum install -y nodejs npm
+#cd /etc/battlebot /urpg-battle-bot
+#cp /etc/battlebot/.env /etc/battlebot/urpg-battle-bot
+#npm install
 #nohup npm run start &
+
+chown -R battlebot:battlebot /etc/battlebot
